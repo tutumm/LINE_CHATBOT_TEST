3ffbaf7eda82e9da6a187cac3d2fd875b57fcd01
Sakan Komolvatin <tutumm96@gmail.com>
2017-07-30 19:31:17 +0700

    2

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],

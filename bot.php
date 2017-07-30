520fe2c3ea2bafd1ee6849275044d0052fc58031
Sakan Komolvatin <tutumm96@gmail.com>
2017-07-30 19:47:42 +0700

    4

--- bot.php
+++ bot.php
@@ -1,28 +1,26 @@
 <?php
 $access_token = 'CPsrfgu1aQf9NvoemSxJql6hUFfSW8xTCmcZOPMqS4kNp3rvCgWmsZe7mT/XNnEpNAP1ZgPHfpQ2SwbV382WMnoNEXDfwkCBuU0KVj6B73I3lqgIPJ+k2EyDXeY5xG8vXcuaRBdpXrLYCo0SpemLWgdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
+
 if (!is_null($events['events'])) {
-	// Loop through each event
+
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['replyToken'];
-			// Get replyToken
+
+			$text = $event;
+
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
+
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
 
-			// Make a POST Request to Messaging API to reply to sender
+
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
